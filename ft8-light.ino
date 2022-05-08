149aceb774b311fec10e765b4481d908229e3e5c
coulisse <corrado.gerbaldo@gmail.com>
2022-05-08 09:03:04 +0200

    decode waiting bit

--- ft8-light.ino
+++ ft8-light.ino
@@ -396,8 +396,8 @@ shared_data data;
 
 void setup() {
 
-    //data.pcm_buffer = (uint8_t** )ps_malloc(1*sizeof(uint8_t));
-  data.pcm_buffer = (uint8_t** )ps_malloc(400000*sizeof(uint8_t));
+
+
 
   Serial.begin(115200);
   log_v("Total heap.: %d", ESP.getHeapSize());
@@ -412,6 +412,19 @@ void setup() {
   u8g2.drawStr(0,10,"Boot...");	// write something to the internal memory
   u8g2.sendBuffer();					// transfer internal memory to the display  
 
+  ar.begin();
+
+  //tasks
+    //data.pcm_buffer = (uint8_t** )ps_malloc(1*sizeof(uint8_t));
+  data.pcm_buffer = (uint8_t** )ps_malloc(400000*sizeof(uint8_t));
+  xEventGroup = xEventGroupCreate();
+  log_v("init tsk_record");
+  xTaskCreatePinnedToCore(tsk_record,"recording task",30000,&data,1,&xHandleRecord,1); 
+  delay(15000);
+  log_v("init tsk_decode");
+  xTaskCreatePinnedToCore(tsk_decode,"decoding  task",40000,&data,1,&xHandleDecode,0);   
+  delay(15000);
+
 
   //objects init
   if (!tm.align_timer()){
@@ -420,56 +433,90 @@ void setup() {
       //stop here
     }
   };
-  ar.begin();
+
+
 }
+
+
+void tsk_record(void * parameters) {  
+
+
+   for (;;) {
+        shared_data* data = (shared_data*) parameters;
+        //log_v("malloc buffer");
+        //free(data->pcm_buffer);
+        //data->pcm_buffer = (uint8_t** )ps_malloc(1*sizeof(uint8_t));
+        //ps_realloc(data->pcm_buffer,1*sizeof(uint8_t));
+        
+        log_v("recording..");
+        ar.record(RECORDING_TIME,(uint8_t*&) data->pcm_buffer,data->rate,data->recording_time,data->bytes_read);
+        //ar.record(RECORDING_TIME,(uint8_t*&) parameters,r,rt,br);
+        log_v("rate %d, recording time %d, bytes read %d", data->rate,data->recording_time,data->bytes_read);
+    //    vTaskDelete(NULL);
+        log_v("setting bit for decoding...");
+        xEventGroupSetBits(xEventGroup,TASKRECORD_BIT); //Event,bit
+        log_v("Suspending recording task..."); 
+        vTaskSuspend(NULL);
+
+        log_v("Recording task resumed"); 
+    }
+}
+
 
 
 void tsk_decode(void * parameters) {  
 
+    const EventBits_t xBitsToWaitFor = TASKRECORD_BIT;
+    EventBits_t xEventGroupValue;
 
-  //get parameters
-  shared_data * data = (shared_data* ) parameters;
+    for (;;) {
+        xEventGroupValue = xEventGroupWaitBits(xEventGroup,xBitsToWaitFor,pdTRUE,pdTRUE,portMAX_DELAY);//EventGroup,receive, clear on exit, all bits?,wait
+        if(xEventGroupValue & TASKRECORD_BIT != 0) {        
+            //get parameters
+            shared_data * data = (shared_data* ) parameters;
 
-  size_t bytes_read =  data->bytes_read;
-  int recording_time =  data->recording_time;
-  int rate = data->rate;
-  uint8_t *lcl_pcm_buffer = (uint8_t* )ps_malloc(bytes_read);  
+            size_t bytes_read =  data->bytes_read;
+            int recording_time =  data->recording_time;
+            int rate = data->rate;
+            uint8_t *lcl_pcm_buffer = (uint8_t* )ps_malloc(bytes_read);  
 
-  memcpy(lcl_pcm_buffer,data->pcm_buffer,bytes_read);
-  //free(data->pcm_buffer);
-  vTaskDelay(1);  // one tick delay (15ms) in between reads for stability 
+            memcpy(lcl_pcm_buffer,data->pcm_buffer,bytes_read);
+            //free(data->pcm_buffer);
+            vTaskDelay(1);  // one tick delay (15ms) in between reads for stability 
 
-  
-#ifdef WRITEWAV 
-  String file_name=FILE_WAV_PATH+FILE_WAV_PREFIX+millis()+FILE_WAV_SUFFIX;
-  char * file_name_c = new char[file_name.length() + 1];
-  strcpy(file_name_c,file_name.c_str()); 
+            
+            #ifdef WRITEWAV 
+            String file_name=FILE_WAV_PATH+FILE_WAV_PREFIX+millis()+FILE_WAV_SUFFIX;
+            char * file_name_c = new char[file_name.length() + 1];
+            strcpy(file_name_c,file_name.c_str()); 
 
-  //create header
-  int headerSize = 44;  
-  byte header[headerSize]; 
-  CreateWavHeader(header, (recording_time*rate/1000*2)-1);
+            //create header
+            int headerSize = 44;  
+            byte header[headerSize]; 
+            CreateWavHeader(header, (recording_time*rate/1000*2)-1);
 
-  //writing file
-  File file = SD.open(file_name_c, FILE_WRITE);
-  if (!file) {
-    log_e("Could not write file");
-    vTaskDelete(NULL);
-  }    
-  file.write(header, headerSize);
-  file.write(lcl_pcm_buffer, bytes_read);    
-  log_d("PCM Bytes: %d", bytes_read);
-  file.close();  
+            //writing file
+            File file = SD.open(file_name_c, FILE_WRITE);
+            if (!file) {
+                log_e("Could not write file");
+                vTaskDelete(NULL);
+            }    
+            file.write(header, headerSize);
+            file.write(lcl_pcm_buffer, bytes_read);    
+            log_d("PCM Bytes: %d", bytes_read);
+            file.close();  
 
-  log_i ("recorded: %s", file_name_c);
+            log_i ("recorded: %s", file_name_c);
 
- #endif
-//  vTaskDelay(50);
-  decode_ft8(lcl_pcm_buffer,bytes_read,rate);
-  log_v("decoded");
-  //free(lcl_pcm_buffer);  
-  log_v("end task"); 
-  vTaskDelete(NULL);
+            #endif
+            //  vTaskDelay(50);
+            decode_ft8(lcl_pcm_buffer,bytes_read,rate); //TODO: replace with data-> psd??
+            log_v("decoded");
+            free(lcl_pcm_buffer);  
+            log_v("end task"); 
+            //vTaskSuspend(NULL);
+        }
+    }
 }
 
 
@@ -486,12 +533,13 @@ void loop() {
 
       if (phase==decode) {
         //TODO: record in a separate task
-        log_v("before recording");
-        ar.record(RECORDING_TIME,(uint8_t*&) data.pcm_buffer,data.rate,data.recording_time,data.bytes_read);
+        log_v("before resume");
+        vTaskResume(xHandleRecord);
+        //ar.record(RECORDING_TIME,(uint8_t*&) data.pcm_buffer,data.rate,data.recording_time,data.bytes_read);
         log_v("before create task decode");
         //xTaskCreatePinnedToCore(tsk_decode,"decodint task",40000,&data,1,NULL,0); 
-        xTaskCreatePinnedToCore(tsk_decode,"decoding task",40000,&data,1,&xHandleDecode,0); 
-        delay(80); //TODO: remove delay
+        //xTaskCreatePinnedToCore(tsk_decode,"decoding task",40000,&data,1,&xHandleDecode,0); 
+        //delay(80); //TODO: remove delay
 
 
       } else if (phase == encode) {
